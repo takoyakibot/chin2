4c83b8fd60f492ac5b0f62399cea52ce53ca8c49
ragingalpaca <lamune.cider@gmail.com>
2023-07-29 09:31:05 +0900

    セッションへの保存

--- src/CreateQuiz.js
+++ src/CreateQuiz.js
@@ -1,53 +1,34 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // useHistoryからuseNavigateに変更
-import './style.css'; // 共通のスタイルシートを読み込む
+import React, { useState, useEffect } from 'react';
+import './style.css'; // スタイルシートをインポート
 
 const CreateQuiz = () => {
-  const navigate = useNavigate(); // useHistoryからuseNavigateに変更
-  const [quizData, setQuizData] = useState([]);
-  const [question, setQuestion] = useState('');
-  const [answer, setAnswer] = useState('');
+  const [selectedImage, setSelectedImage] = useState(null);
 
-  const handleAddQuiz = () => {
-    const newQuizData = [...quizData, { question, answer }];
-    setQuizData(newQuizData);
-    setQuestion('');
-    setAnswer('');
-  };
+  // ページロード時にセッションストレージから保存された画像パス情報を取得
+  useEffect(() => {
+    const savedImagePath = sessionStorage.getItem('selectedImagePath');
+    if (savedImagePath) {
+      setSelectedImage(savedImagePath);
+    }
+  }, []);
 
-  const handleSaveQuiz = () => {
-    // ここでクイズデータを保存するAPIを呼び出す処理を書く（モックの場合は省略）
-    console.log('クイズデータを保存しました:', quizData);
-    navigate('/'); // useHistoryからnavigateに変更
+  const handleImageSelect = (event) => {
+    const imageFile = event.target.files[0];
+    const imageUrl = URL.createObjectURL(imageFile);
+    setSelectedImage(imageUrl);
+    // 選択した画像のパス情報をセッションストレージに保存
+    sessionStorage.setItem('selectedImagePath', imageUrl);
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>クイズ作成</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="質問"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="答え"
-          value={answer}
-          onChange={(e) => setAnswer(e.target.value)}
-        />
-        <button onClick={handleAddQuiz}>追加</button>
+      <div className="image-select">
+        {/* 画像選択フィールドの画像表示領域 */}
+        {selectedImage && <img src={selectedImage} alt="Selected" />}
+        {/* 画像選択ボタン */}
+        <input type="file" onChange={handleImageSelect} accept="image/*" />
       </div>
-      <div>
-        <h2>クイズ一覧</h2>
-        {quizData.map((quiz, index) => (
-          <div key={index}>
-            <span>{index + 1}. 質問: {quiz.question}, 答え: {quiz.answer}</span>
-          </div>
-        ))}
-      </div>
-      <button onClick={handleSaveQuiz}>保存</button>
     </div>
   );
 };
