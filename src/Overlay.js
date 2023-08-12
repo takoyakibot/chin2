f37c8dd9c95c1923e670ce03b2d552c88e6134e0
ragingalpaca <lamune.cider@gmail.com>
2023-08-12 18:33:13 +0900

    gifのリセット追加

--- src/Overlay.js
+++ src/Overlay.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from 'react';
 import './overlay.css';
 
 const Overlay = ({ correctAnswer, showOverlay, onCloseOverlay }) => {
+  const [gifName, setGifName] = useState(correctAnswer);
 
   // オーバーレイがクリックされたときに閉じる処理
   const handleClickOverlay = (event) => {
     // オーバーレイの外側をクリックした場合は閉じる
     if (event.target.classList.contains('overlay')) {
+      // URLを変更する
+      setGifName('');
+
       onCloseOverlay();
     }
   };
@@ -16,7 +20,7 @@ const Overlay = ({ correctAnswer, showOverlay, onCloseOverlay }) => {
       <div className="overlay-content">
         <div className="close-button" onClick={onCloseOverlay}>x</div>
         <div className="overlay">
-          <img src={process.env.PUBLIC_URL + '/images/' + correctAnswer + '.gif?start=1'} alt="Animation" />
+          <img src={process.env.PUBLIC_URL + '/images/' + gifName + '.gif?start=1'} alt="Animation" />
         </div>
       </div>
     </div>
