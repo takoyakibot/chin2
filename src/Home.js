54f9d8ccca1cd09b6ab8f974d68ec19cd0a075cc
ragingalpaca <lamune.cider@gmail.com>
2023-08-12 20:11:13 +0900

    日付の調整

--- src/Home.js
+++ src/Home.js
@@ -19,19 +19,44 @@ const QuizListPage = () => {
     sessionStorage.setItem('savedData', JSON.stringify(quiz));
   };
 
-  const answerQuiz = (quiz) => {
+  const answerQuiz = (quiz, index) => {
+    const updatedQuizInfo = [...quizzes];
+    const currentDate = new Date().toISOString(); // 現在日時を取得
+    updatedQuizInfo[index].answerDate = currentDate; // answerDateを更新
+    localStorage.setItem('quizInfo', JSON.stringify(updatedQuizInfo)); // ローカルストレージに再保存
+  
     sessionStorage.setItem('answerData', JSON.stringify(quiz));
+    setQuizzes(updatedQuizInfo); // ステートを更新
   };
 
   const createNewQuiz = () => {
     sessionStorage.removeItem('savedData');
   };
 
+  const formatDate = (dateString) => {
+    if (dateString === undefined) {
+      return '';
+    }
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    return (
+      <>
+        {`${year}/${month}/${day}`}<br />
+        {`${hours}:${minutes}:${seconds}`}
+      </>
+    );
+  };
+  
   const resumeButton = (
     <button disabled={answerData ? false : true } className="btn btn-warning resume-quiz-button">
       回答を再開
     </button>
-);
+  );
 
   return (
     <div className="container">
@@ -48,24 +73,23 @@ const QuizListPage = () => {
       <div className="m-2">
         <div className="row quiz-header">
           <div className="col col-1">No</div>
-          <div className="col col-2">登録名</div>
+          <div className="col col-3">登録名</div>
           <div className="col col-1">付箋</div>
           <div className="col col-1"></div>
           <div className="col col-2">登録日</div>
           <div className="col col-1">編集</div>
           <div className="col col-2">回答日</div>
-          <div className="col col-1">正解数</div>
           <div className="col col-1">回答</div>
         </div>
         {quizzes.map((quiz, index) => (
         <div key={index} className="row quiz-item">
           <div className="col col-1 d-flex align-items-center justify-content-center">{index + 1}</div>
-          <div className="col col-2 d-flex align-items-center justify-content-center">{quiz.quizName}</div>
+          <div className="col col-3 d-flex align-items-center justify-content-center">{quiz.quizName}</div>
           <div className="col col-1 d-flex align-items-center justify-content-center">{quiz.quizInfo.length}</div>
           <div className="col col-1 d-flex align-items-center justify-content-center">
             <img src={quiz.thumbnail} alt="quiz thumbnail" className="thumbnail"/>
           </div>
-          <div className="col col-2 d-flex align-items-center justify-content-center">{quiz.updDate}</div>
+          <div className="col col-2 d-flex align-items-center justify-content-center">{formatDate(quiz.updDate)}</div>
           <div className="col col-1 d-flex align-items-center justify-content-center">
             <Link to="/create">
               <button className="btn btn-success" onClick={() => editQuiz(quiz)}>
@@ -73,10 +97,10 @@ const QuizListPage = () => {
               </button>
             </Link>
           </div>
-          <div className="col col-2 d-flex align-items-center justify-content-center">{quiz.answeredDate}</div>
+          <div className="col col-2 d-flex align-items-center justify-content-center">{formatDate(quiz.answerDate)}</div>
           <div className="col col-1 d-flex align-items-center justify-content-center">
             <Link to="/answer">
-              <button className="btn btn-danger" onClick={() => answerQuiz(quiz)}>
+              <button className="btn btn-danger" onClick={() => answerQuiz(quiz, index)}>
                 回答
               </button>
             </Link>
